02cffd522c8ffd5a0606470c28343eb1ae932532
Predrag <predrag.tosic@elfak.rs>
2023-06-13 20:50:24 +0200

    Controllers

---
+++ Telekomunikaciona_Kompanija_Web_API/Telekomunikaciona_Kompanija_Web_API/Controllers/InternetController.cs
@@ -0,0 +1,85 @@
+﻿using DatabaseAccess.DTOs;
+using DatabaseAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Telekom_Kompanija_Web_API.Controllers
+{
+    public class InternetController : Controller
+    {
+        [HttpGet("VratiInternete")]
+        public IActionResult VratiInternete()
+        {
+            try
+            {
+                IList<InternetView> tel = new List<InternetView>();
+                tel = DataProvider.VratiInternete();
+
+                return Ok(tel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("SacuvajInternet")]
+        public IActionResult SacuvajInternet([FromBody] InternetView net, OstvareniProtokView placanje, FlatRateView pl)
+        {
+            try
+            {
+                DataProvider.SacuvajInternet(net, placanje, pl);
+
+                return Ok("Uspesno dodat internet.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("VratiInternet/{id}")]
+        public IActionResult VratiInternet(int id)
+        {
+            try
+            {
+                InternetView net=new InternetView();
+                net=DataProvider.VratiInternet(id);
+
+                return Ok(net);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("IzmeniInternet")]
+        public IActionResult IzmeniInternet([FromBody]InternetView net)
+        {
+            try
+            {
+                DataProvider.IzmeniInternet(net);
+
+                return Ok("Uspesno izmenjen internet.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("ObrisiInternet/{id}")]
+        public IActionResult ObrisiInternet(int id)
+        {
+            try
+            {
+                DataProvider.ObrisiInternet(id);
+
+                return Ok("Uspesno obrisan internet.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Telekomunikaciona_Kompanija_Web_API/Telekomunikaciona_Kompanija_Web_API/Controllers/TelefonijaController.cs
@@ -0,0 +1,67 @@
+﻿using DatabaseAccess;
+using DatabaseAccess.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Telekom_Kompanija_Web_API.Controllers
+{
+    public class TelefonijaController : Controller
+    {
+        [HttpPut("SacuvajTelefoniju")]
+        public IActionResult SacuvajTelefoniju([FromBody]TelefonijaView tel)
+        {
+            try
+            {
+                DataProvider.SacuvajTelefoniju(tel);
+
+                return Ok("Uspesno sacuvali telefoniju.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("VratiTelefoniju/{tel}")]
+        public IActionResult VratiTelefoniju(int tel)
+        {
+            try
+            {
+                TelefonijaView te=new TelefonijaView();
+                te=DataProvider.VratiTelefoniju(tel);
+
+                return Ok(te);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("IzmeniTelefoniju")]
+        public IActionResult IzmeniTelefoniju([FromBody]TelefonijaView tel)
+        {
+            try
+            {
+                DataProvider.IzmeniTelefoniju(tel);
+
+                return Ok("Uspesno azurirana telefonija.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("ObrisiTelefoniju/{tel}")]
+        public IActionResult ObrisiTelefoniju(int tel)
+        {
+            try
+            {
+                DataProvider.ObrisiTelefoniju(tel);
+
+                return Ok("Uspesno obrisana telefonija.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
