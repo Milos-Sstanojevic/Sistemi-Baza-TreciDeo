592588fd3baecf8d0f9916e466b86d118364970c
Predrag <predrag.tosic@elfak.rs>
2023-06-13 15:29:31 +0200

    DTOs

---
+++ Telekomunikaciona_Kompanija_Web_API/DatabaseAccess/DTOs/DodatniPaketKanalaView.cs
@@ -0,0 +1,16 @@
+﻿using Telekomunikaciona_Kompanija_NHibernate.Entiteti;
+
+namespace DatabaseAccess.DTOs
+{
+    public class DodatniPaketKanalaView
+    {
+        public virtual int Id { get; set; }
+        public virtual string DodatniPaket { get; set; }
+        public virtual TelevizijaView Televizija { get; set; }
+        public DodatniPaketKanalaView(DodatniPaketKanala paket)
+        {
+            Id = paket.Id;
+            DodatniPaket=paket.DodatniPaket;
+        }
+    }
+}
---
+++ Telekomunikaciona_Kompanija_Web_API/DatabaseAccess/DTOs/StatickaAdresaView.cs
@@ -0,0 +1,16 @@
+﻿using Telekomunikaciona_Kompanija_NHibernate.Entiteti;
+
+namespace DatabaseAccess.DTOs
+{
+    public class StatickaAdresaView
+    {
+        public int Id { get; set; }
+        public string Staticka_Adresa { get; set; }
+        public FlatRateView FlatRate { get; set; }
+        public StatickaAdresaView(StatickaAdresa sa)
+        {
+            Id = sa.Id;
+            Staticka_Adresa=sa.Staticka_Adresa;
+        }
+    }
+}
